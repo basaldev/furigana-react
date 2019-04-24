09f4f48e620cc520b50210c571dd9d3dc67c1f08
Alex <alex@fs276ed17e.tkyc008.ap.nuro.jp>
2019-04-24 09:17:57 +0900

    show furigana now

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,9 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent furigana="わたし:だいす">
+          私はクッキーが大好きです。
+        </ExampleComponent>
       </div>
     )
   }
--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,95 @@
  */
 
 import * as React from 'react'
-
+import nihongo from 'nihongo';
 import styles from './styles.css'
 
-export type Props = { text: string }
+export type Props = {
+  furigana: string;
+  children: any;
+}
+export type defaultState = {
+  foundKanji: Array<any>,
+  furigana: string;
+  renderedString: any
+}
+export type MatchedKanji = Array<{
+  kanji: string,
+  furigana: string
+}>
 
-export default class ExampleComponent extends React.Component<Props> {
+export type ParsedKanji = {
+  foundKanji: any, furigana: string[]
+}
+
+export default class Furigana extends React.Component<Props> {
+  state: defaultState;
+  props: Props;
+  constructor(props:Props){
+    super(props);
+    this.state = {
+      furigana: '',
+      foundKanji: [],
+      renderedString: null
+    }
+  }
+  isText(text: string){
+    return typeof text === 'string';
+  }
+  buildFuri(){
+    const parsed = this.parseChild();
+    const matched = this.matchKanji(parsed);
+    const cutChildren = this.cutChildren(matched);
+    const renderString = this.buildRender(cutChildren, matched);
+    return renderString;
+  }
+  parseChild() {
+    const { children } = this.props;
+    if(this.isText(children)){
+      return {
+        foundKanji: nihongo.parseKanjiCompounds(children),
+        furigana: this.props.furigana.split(':')
+      }
+    } else {
+      console.error('only text for now');
+      return {
+        foundKanji: [],
+        furigana: []
+      };
+    }
+  }
+  matchKanji(parsed: ParsedKanji){
+    const { foundKanji, furigana } = parsed;
+    return foundKanji.map((kanji:string, i:number) => {
+      //TODO add kanji length here
+      return { kanji, furigana: furigana[i] }
+    })
+  }
+  cutChildren(matchedKanji: MatchedKanji){
+    const text = this.props.children;
+    return matchedKanji.map((item) => {
+      const beforeChar = text.split(item.kanji)[0];
+      const length = nihongo.parseKanji(item.kanji).length;
+      return beforeChar.substr(length);
+    })
+  }
+  buildRender(cutChildren: string[], matchedKanji: MatchedKanji){
+    return (<>{matchedKanji.map((item, i) => {
+      debugger
+      return (<><span className="kanji">
+        <sup>{item.furigana}</sup>
+        <span>{item.kanji}</span>
+      </span><span>{cutChildren[i]}</span></>)
+    })}</>)
+  }
+  componentWillMount(){
+
+  }
   render() {
-    const {
-      text
-    } = this.props
-
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <span className={styles.test}>
+        {this.buildFuri()}
+      </span>
     )
   }
 }
